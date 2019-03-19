20a443009b9c71e982b2841a4de70e077c75640c
JACOBIN-SCTCS <jacobja3@gmail.com>
2019-03-19 16:37:57 +0530

    Added comments to exp7

--- exp7/client.c
+++ exp7/client.c
@@ -1,3 +1,4 @@
+//JACOB JAMES K  (31) EXP 7. IPC MESSAGE QUEUE
 #include<stdio.h>
 #include<sys/msg.h>
 #include<sys/wait.h>
@@ -10,20 +11,39 @@ struct msgbuf
 
 int main()
 {
+	// MESSAGE TO BE SEND
 	struct msgbuf msg1 = { mtype: 1 , message:"Hello "};
 	struct msgbuf msg2 = { mtype: 2 , message:"World "};
 	struct msgbuf msg3;
 	int msgid;
+
+	//GET MESSAGE QUEUE
 	msgid = msgget(3712,IPC_CREAT|0666);
+
+	//SEND MESSAGES
 	msgsnd(msgid,&msg1,sizeof(msg1),0);
 	msgsnd(msgid,&msg2,sizeof(msg2),0);
 	printf("Client has send the messages\n");
 
-
+	//RECEIVE CONCATENATED MESSAGE
 	msgrcv(msgid,&msg3,sizeof(msg3),3,0);
 	printf("Client has got the message %s\n",msg3.message);
+	// DETACH MESSSAGE QUEUE
 	msgctl(msgid,IPC_RMID,NULL);
 
 	return 0;
 
 }
+
+/*
+
+OUTPUT
+_____________________
+
+Client1 message = Hello
+Client2 message = World
+
+MESSAGE = Hello World
+SERVER HAS SEND DATA
+
+*/
--- exp7/server.c
+++ exp7/server.c
@@ -1,3 +1,4 @@
+// JACOB JAMES K (31) PROGRAM 7 .IPC MESSAGE QUEUE
 #include <stdio.h>
 #include <sys/msg.h>
 #include <string.h>
@@ -18,25 +19,36 @@ void main()
 
 	char mess[40];
 
+	//CREATE  A MESSAGE QUEUE
 	msgid =msgget(3712,IPC_CREAT|0666);
 
-
+	//RECEIVE MESSAGES FROM CLIENTS
 	msgrcv(msgid,&msg1,sizeof(msg1),1,0);
  	msgrcv(msgid,&msg2,sizeof(msg2),2,0);
 
 	printf("Client1 message = %s\n",msg1.message);
 	printf("Client2 message = %s\n",msg2.message);
 
+	//CONCATENATING MESSAGES
 	msg3.mtype =3;
 	strcpy(mess ,msg1.message);
         strcat(mess,msg2.message);
         strcpy(msg3.message , mess);
 
 	printf("\nMESSAGE = %s",mess);
+	//MESSAGE TO SEND TO CLIENT
 	msgsnd(msgid,&msg3,sizeof(msg3),0);
 	printf("\nSERVER HAS SEND DATA \n");
+	//DETACH SHARED MEMORY
 	msgctl(msgid,IPC_RMID,NULL);
 	//return 0;
 
 }
 
+/*
+
+OUTPUT
+________________________________
+Client has send the messages
+Client has got the message Hello World 
+*/
