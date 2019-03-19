b464d9ecf5fd0ff3a15050b01bb3d9ed4671e08c
JACOBIN-SCTCS <jacobja3@gmail.com>
2019-03-19 16:28:18 +0530

    Completed Message queue

--- exp7/server.c
+++ exp7/server.c
@@ -2,6 +2,7 @@
 #include <sys/msg.h>
 #include <string.h>
 
+
 struct msgbuf
 {
         long mtype;
@@ -9,21 +10,33 @@ struct msgbuf
 } ;
 
 
-int main()
+void main()
 {
 	int msgid;
 	struct msgbuf msg1 , msg2;
+	struct msgbuf msg3;
+
+	char mess[40];
 
 	msgid =msgget(3712,IPC_CREAT|0666);
 
-	msgrcv(msgid,&msg1,sizeof(msg1),1,0);
- 	msgrcv(msgid,&msg2,sizeof(msg2),1,0);
 
-	strcat(msg1.message, msg2.message);
-	msgsnd(msgid,&msg1,sizeof(msg1),0);
-	printf("SERVER HAS SEND DATA \n");
+	msgrcv(msgid,&msg1,sizeof(msg1),1,0);
+ 	msgrcv(msgid,&msg2,sizeof(msg2),2,0);
+
+	printf("Client1 message = %s\n",msg1.message);
+	printf("Client2 message = %s\n",msg2.message);
+
+	msg3.mtype =3;
+	strcpy(mess ,msg1.message);
+        strcat(mess,msg2.message);
+        strcpy(msg3.message , mess);
+
+	printf("\nMESSAGE = %s",mess);
+	msgsnd(msgid,&msg3,sizeof(msg3),0);
+	printf("\nSERVER HAS SEND DATA \n");
 	msgctl(msgid,IPC_RMID,NULL);
-	return 0;
+	//return 0;
 
 }
 
